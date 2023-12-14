cc23275813104e9e0cfc3d0bf03216b6096649c3
Benzazoua <billalben252@gmail.com>
2023-12-14 14:35:35 +0100

    change home shapes when click on color buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,18 @@ window.onload = () => {
     preloader.style.display = "none";
   }, 3000);
 };
+
+//  =========== change home shapes when click on color buttons ===========
+const colorButtons = document.querySelector(".home__color-buttons");
+
+colorButtons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("home__color-button")) {
+    colorButtons.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    document.querySelector(".home__shape.active").classList.remove("active");
+    document
+      .querySelector(`.home__shape.${e.target.id}`)
+      .classList.add("active");
+  }
+});
