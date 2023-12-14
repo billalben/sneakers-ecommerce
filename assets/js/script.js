afe700a00e3dc5cddd4bedd94ab015650110680f
Benzazoua <billalben252@gmail.com>
2023-12-14 13:29:12 +0100

    Create Preloader && Header

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//  ================= PRELOADER =================
+onload = () => {
+  const preloader = document.getElementById("preloader");
+
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 3000);
+};
